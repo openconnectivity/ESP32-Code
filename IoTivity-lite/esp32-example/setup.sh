a7d97c353bb3241ce58eedb9ffc4e6896057f0d2
Clarke Stevens <clarke.stevens@sjrb.ca>
2020-10-21 09:40:00 -0600

    Update setup.sh

--- IoTivity-lite/esp32-example/setup.sh
+++ IoTivity-lite/esp32-example/setup.sh
@@ -4,6 +4,7 @@ PROJNAME=${PWD##*/}
 
 cp -f ${EXAMPLEPATH}/ESP32-Code/IoTivity-lite/esp32-example/devbuildmake ${CURPWD}/devbuildmake
 cp -f ${EXAMPLEPATH}/ESP32-Code/IoTivity-lite/esp32-example/esp32-example-config.json ${CURPWD}/${PROJNAME}-config.json
-cp ${EXAMPLEPATH}/ESP32-Code/arduino/esp32-example/esp32-example.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/ESP32-Code/esp32/esp32-example/esp32-example.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/ESP32-Code/IoTivity-lite/esp32-example/esp32-example.c ${CURPWD}/src/${PROJNAME}.c
 cp ${EXAMPLEPATH}/ESP32-Code/IoTivity-lite/esp32-example/esp32-example-main.c ${CURPWD}/src/${PROJNAME}-main.c
+cp ${EXAMPLEPATH}/ESP32-Code/IoTivity-lite/esp32-example/pki_certs.h ${CURPWD}/src/pki_certs.h
